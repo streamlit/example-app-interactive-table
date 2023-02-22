b250e9e5107d869ff69bc935866303d41659d967
Jacek <jacek@gooddata.com>
2023-02-22 16:12:34 +0100

    Replace missing theme in AgGrid

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ def aggrid_interactive_table(df: pd.DataFrame):
         df,
         enable_enterprise_modules=True,
         gridOptions=options.build(),
-        theme="light",
+        theme="streamlit",
         update_mode=GridUpdateMode.MODEL_CHANGED,
         allow_unsafe_jscode=True,
     )
